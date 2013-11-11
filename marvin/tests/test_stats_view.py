b49fb651165189f2fc2850a1c3ea57e337080438
Tarjei Husøy <tarjei@roms.no>
2013-11-11 20:07:27 +0100

    Delint and clean up object creation for stats tests.

--- marvin/tests/test_stats_view.py
+++ marvin/tests/test_stats_view.py
@@ -24,6 +24,8 @@ def rendered_context(app):
 class StatsViewTest(TestCaseWithTempDB):
 
     def setUp(self):
+
+        # Add movies
         avatar = Movie(
             title='Avatar',
             external_id='imdb:tt0499549',
@@ -33,26 +35,77 @@ class StatsViewTest(TestCaseWithTempDB):
             external_id='imdb:tt1440129',
         )
 
-        avatar_sins = Stream(name='CinemaSins', movie=avatar)
-        avatar_actors = Stream(name='Actors Introduced', movie=avatar)
-        battleship_sins = Stream(name='CinemaSins', movie=battleship)
+        # Add streams
+        avatar_sins = Stream(
+            name='CinemaSins',
+            movie=avatar,
+        )
+        avatar_actors = Stream(
+            name='Actors Introduced',
+            movie=avatar,
+        )
+        battleship_sins = Stream(
+            name='CinemaSins',
+            movie=battleship,
+        )
 
-        avatar_fail_intro = Entry(entry_point_in_ms=5*60*1000, title='<h1>Title</h1>', content='Massive TVs!', stream=avatar_sins)
-        avatar_fail_middle = Entry(entry_point_in_ms=45*60*1000, title='<h1>Title</h1>', content='Trees without oxygen?',
-            stream=avatar_sins)
-        avatar_fail_end = Entry(entry_point_in_ms=90*60*1000, title='<h1>Title</h1>', content='Oral flower sex', stream=avatar_sins)
+        # Add entries
+        avatar_fail_intro = Entry(
+            entry_point_in_ms=5*60*1000,
+            title='<h1>Title</h1>',
+            content='Massive TVs!',
+            stream=avatar_sins,
+        )
+        avatar_fail_middle = Entry(
+            entry_point_in_ms=45*60*1000,
+            title='<h1>Title</h1>',
+            content='Trees without oxygen?',
+            stream=avatar_sins,
+        )
+        avatar_fail_end = Entry(
+            entry_point_in_ms=90*60*1000,
+            title='<h1>Title</h1>',
+            content='Oral flower sex',
+            stream=avatar_sins,
+        )
 
-        avatar_actor_sam = Entry(entry_point_in_ms=1*60*1000, title='<h1>Title</h1>',
-            content='Sam Worthington, Clash of the Titans, Terminator Salvation', stream=avatar_actors)
-        avatar_actor_sigourney = Entry(entry_point_in_ms=3*60*1000, title='<h1>Title</h1>',
-            content='Sigourney Weaver, Alien, Aliens, Ghostbusters', stream=avatar_actors)
+        avatar_actor_sam = Entry(
+            entry_point_in_ms=1*60*1000,
+            title='<h1>Title</h1>',
+            content='Sam Worthington, Clash of the Titans, Terminator Salvation',
+            stream=avatar_actors,
+        )
+        avatar_actor_sigourney = Entry(
+            entry_point_in_ms=3*60*1000,
+            title='<h1>Title</h1>',
+            content='Sigourney Weaver, Alien, Aliens, Ghostbusters',
+            stream=avatar_actors,
+        )
 
-        battleship_sin_intro = Entry(entry_point_in_ms=20*1000, title='<h1>Title</h1>', content='Hasbro films', stream=battleship_sins)
-        battleship_sin_early_intro = Entry(entry_point_in_ms=60*1000, title='<h1>Title</h1>', content='Believes in wishes',
-            stream=battleship_sins)
-        battleship_sin_middle = Entry(entry_point_in_ms=5*60*1000, title='<h1>Title</h1>', content='No skype lag', stream=battleship_sins)
-        battleship_sin_outro = Entry(entry_point_in_ms=90*60*1000, title='<h1>Title</h1>', content='No one says "You sunk my battleship"',
-            stream=battleship_sins)
+        battleship_sin_intro = Entry(
+            entry_point_in_ms=20*1000,
+            title='<h1>Title</h1>',
+            content='Hasbro films',
+            stream=battleship_sins,
+        )
+        battleship_sin_early_intro = Entry(
+            entry_point_in_ms=60*1000,
+            title='<h1>Title</h1>',
+            content='Believes in wishes',
+            stream=battleship_sins,
+        )
+        battleship_sin_middle = Entry(
+            entry_point_in_ms=5*60*1000,
+            title='<h1>Title</h1>',
+            content='No skype lag',
+            stream=battleship_sins,
+        )
+        battleship_sin_outro = Entry(
+            entry_point_in_ms=90*60*1000,
+            title='<h1>Title</h1>',
+            content='No one says "You sunk my battleship"',
+            stream=battleship_sins,
+        )
 
         self.addItems(
             avatar,
